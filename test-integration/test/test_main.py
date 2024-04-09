f4f62e87ae109fa2d2258b47f26413a8a5841f8d
Madhurendra Nath Tiwari <madhurendraakela123@gmail.com>
2024-04-09 11:58:04 +0530

    Delete test-integration directory

--- test-integration/test/test_main.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from main import greet
-
-class TestGreet(unittest.TestCase):
-    def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World!")
-
-if __name__ == "__main__":
-    unittest.main()
